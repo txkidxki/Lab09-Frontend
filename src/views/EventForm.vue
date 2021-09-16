7026eaa702184c3e8e4d56024d99d24dafa37bfc
txkidxki <rattanakorn_re@cmu.ac.th>
2021-09-16 16:52:48 +0700

    5.3

--- src/views/EventForm.vue
+++ src/views/EventForm.vue
@@ -42,12 +42,10 @@
 </template>
 <script>
 import EventService from '@/services/EventService.js'
-import BaseInput from '@/components/BaseInput'
+
 export default {
   inject: ['GStore'],
-    components: {
-      BaseInput
-    },
+   
   data() {
     return {
       event: {
